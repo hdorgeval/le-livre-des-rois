52c1c20cb5cba81039e7c6f2fb915af540b5fe2d
hdorgeval <henri.dorgeval@outlook.fr>
2019-10-31 21:13:32 +0100

    feat(gatsby):  be able to handle images in markdown files

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,24 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    'gatsby-transformer-remark',
+    'gatsby-plugin-sharp',
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 960,
+            },
+          },
+        ],
+      },
+    },
+    'gatsby-transformer-sharp',
     'gatsby-plugin-dark-mode',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-sass',
