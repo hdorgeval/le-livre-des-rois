879215f5aa4d08920c39bd1dc104a216f3c26edb
hdorgeval <henri.dorgeval@outlook.fr>
2019-12-27 22:45:53 +0100

    fix(build): remove unused import

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,6 +1,5 @@
 import styles from './header.module.scss';
 import { DarkModeToggle } from './dark-mode-toggle/dark-mode-toggle';
-import { GithubButton } from './github-button/github-button';
 import { HomeButton } from './home-button/home-button';
 import { TagsButton } from './tags-button/tags-button';
 import { GenealogiesButton } from './genealogies-button/genealogies-button';
