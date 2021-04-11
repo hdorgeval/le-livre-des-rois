e2807f4479a38f05f8ef47bf3aec9ff5443c5a9a
hdorgeval <henri.dorgeval@outlook.fr>
2021-04-11 19:29:38 +0200

    refactor(web): refactor the markdown template

--- src/templates/markdown-template/markdown-template.tsx
+++ src/templates/markdown-template/markdown-template.tsx
@@ -1,7 +1,8 @@
 import { MarkdownRemarkResponse } from '../../graphql';
-import { Layout, Title, SEO } from '../../components';
+import { Layout, SEO } from '../../components';
 import React from 'react';
 import { graphql } from 'gatsby';
+import './markdown-template.css';
 
 interface MarkdownTemplateProps {
   data: MarkdownRemarkResponse;
@@ -9,25 +10,44 @@ interface MarkdownTemplateProps {
 export const MarkdownTemplate: React.FC<MarkdownTemplateProps> = ({ data }) => {
   const markdownData = data.markdownRemark;
   const firstHeading = markdownData.headings[0].value;
-  const htmlWithoutFirstHeading = markdownData.html.replace(`<h1>${firstHeading}</h1>`, '');
+
+  const note = '<sup id="fnref-1"><a href="#fn-1" class="footnote-ref">1</a></sup>';
+  const hasTitleNote = markdownData.html.includes(`<h1>${firstHeading}${note}</h1>`);
+
+  const htmlWithoutFirstHeading = hasTitleNote
+    ? markdownData.html.replace(`<h1>${firstHeading}${note}</h1>`, '')
+    : markdownData.html.replace(`<h1>${firstHeading}</h1>`, '');
 
   return (
     <Layout>
       <SEO title="Le Livre des Rois - Shâhnâmeh" contentType="website" description={firstHeading} />
       <div>
-        <Title text={firstHeading} subtitle=""></Title>
-        <div
-          style={{
-            width: '100%',
-            height: '200px',
-            backgroundColor: '#fafafa',
-            backgroundImage: `Url(${markdownData.frontmatter.landscape}/960x200)`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-            marginBottom: '30px',
-          }}
-        ></div>
-        <div dangerouslySetInnerHTML={{ __html: htmlWithoutFirstHeading }} />
+        <div className="card bg-dark text-white">
+          <div className="card-header text-light text-center text-uppercase border-bottom border-bottom-1 border-secondary pb-0 mb-3">
+            <h1 className="card-title text-truncate">
+              {firstHeading}
+              {hasTitleNote ? <span dangerouslySetInnerHTML={{ __html: note }}></span> : ''}
+            </h1>
+          </div>
+          <div className="card-body card-text">
+            <div
+              style={{
+                width: '100%',
+                height: '200px',
+                backgroundColor: '#fafafa',
+                backgroundImage: `Url(${markdownData.frontmatter.landscape}/960x200)`,
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat',
+                marginBottom: '30px',
+              }}
+            ></div>
+            <div
+              id="episode-content"
+              dangerouslySetInnerHTML={{ __html: htmlWithoutFirstHeading }}
+            />
+          </div>
+          <div className="card-footer text-muted"></div>
+        </div>
       </div>
     </Layout>
   );
