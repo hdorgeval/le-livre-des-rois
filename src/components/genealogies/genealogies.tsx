13b1e766b18fd37d85e0c369ea846bc5d3ae7594
hdorgeval <henri.dorgeval@outlook.fr>
2021-04-12 00:02:31 +0200

    refactor(web): refactor the genealogy component

--- src/components/genealogies/genealogies.tsx
+++ src/components/genealogies/genealogies.tsx
@@ -1,12 +1,14 @@
-import { GenealogyLohrasp } from './genealogy-lohrasp/genealogy-lohrasp';
-import { GenealogySelm } from './genealogy-selm/genealogy-selm';
-import { GenealogyNewder } from './genealogy-newder/genealogy-newder';
-import { GenealogyGouderz } from './genealogy-gouderz/genealogy-gouderz';
-import { GenealogyZoroastre } from './genealogy-zoroastre/genealogy-zoroastre';
-import { GenealogyArdjasp } from './genealogy-ardjasp/genealogy-ardjasp';
-import { GenealogyDjamasp } from './genealogy-djamasp/genealogy-djamasp';
-import { GenealogyKhosrou } from './genealogy-khosrou/genealogy-khosrou';
-import { GenealogyRustem } from './genealogy-rustem/genealogy-rustem';
+import {
+  GenealogyArdjasp,
+  GenealogyDjamasp,
+  GenealogyGouderz,
+  GenealogyKhosrou,
+  GenealogyLohrasp,
+  GenealogyNewder,
+  GenealogyRustem,
+  GenealogySelm,
+  GenealogyZoroastre,
+} from '.';
 import React from 'react';
 export const Genealogies: React.FC = () => {
   return (
--- src/components/genealogies/genealogy/genealogy.tsx
+++ src/components/genealogies/genealogy/genealogy.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './genealogy.css';
 
 export interface GenealogyProps {
   svgGraph: string;
@@ -9,9 +10,14 @@ export const Genealogy: React.FC<GenealogyProps> = ({ treeName, svgGraph }) => {
   const result = svgGraph.replace(/<style>/gi, '<foo>').replace(/<\/style>/gi, '</foo>');
 
   return (
-    <>
-      <h2>{treeName}</h2>
-      <div dangerouslySetInnerHTML={{ __html: result }}></div>
-    </>
+    <div className="card text-center bg-dark text-white border-secondary mb-4 ms-4 me-4">
+      <div className="card-header">
+        <h5 className="card-title mb-0">{treeName}</h5>
+      </div>
+      <div className="card-body card-text">
+        <div className="mermaid-graph" dangerouslySetInnerHTML={{ __html: result }}></div>
+      </div>
+      <div className="card-footer text-muted"></div>
+    </div>
   );
 };
