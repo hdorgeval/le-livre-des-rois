672b0a9b6bbf69fbb7b6f7d9240cce76cbcafb6a
hdorgeval <henri.dorgeval@outlook.fr>
2021-04-17 00:13:46 +0200

    feat(web): add colors in tags palette

--- src/components/tags/tag/tag.tsx
+++ src/components/tags/tag/tag.tsx
@@ -9,52 +9,8 @@ export interface TagProps {
 }
 export const Tag: React.FC<TagProps> = ({ text, size, index }) => {
   const getStyleByIndex = (i: number) => {
-    const konwnIndex = (i % 20) + 1;
-    switch (konwnIndex) {
-      case 1:
-        return 'color1';
-      case 2:
-        return 'color2';
-      case 3:
-        return 'color3';
-      case 4:
-        return 'color4';
-      case 5:
-        return 'color5';
-      case 6:
-        return 'color6';
-      case 7:
-        return 'color7';
-      case 8:
-        return 'color8';
-      case 9:
-        return 'color9';
-      case 10:
-        return 'color10';
-      case 11:
-        return 'color11';
-      case 12:
-        return 'color12';
-      case 13:
-        return 'color13';
-      case 14:
-        return 'color14';
-      case 15:
-        return 'color15';
-      case 16:
-        return 'color16';
-      case 17:
-        return 'color17';
-      case 18:
-        return 'color18';
-      case 19:
-        return 'color19';
-      case 20:
-        return 'color20';
-
-      default:
-        return 'color1';
-    }
+    const konwnIndex = (i % 22) + 1;
+    return `color${konwnIndex}`;
   };
   return (
     <span
