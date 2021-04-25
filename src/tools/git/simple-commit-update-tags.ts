80d046c4c51904dc86d918c1ac5132d17732d0dd
hdorgeval <henri.dorgeval@outlook.fr>
2021-04-25 15:53:04 +0200

    chore(git): add script to automate tags update commits

---
+++ src/tools/git/simple-commit-update-tags.ts
@@ -0,0 +1,37 @@
+import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
+import path from 'path';
+import {} from 'fs';
+
+const options: Partial<SimpleGitOptions> = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+};
+
+async function autoCommitEpisodesWithUpdatedTags(sourceFolder: string) {
+  const search = sourceFolder.match(/\/\d\d-(.*)\//i);
+  const reign = search ? search[1] : 'reign';
+
+  // when setting all options in a single object
+  const git: SimpleGit = simpleGit(options);
+
+  const status = await git.status();
+  // eslint-disable-next-line no-console
+  console.log(status);
+  const unstagedFiles = status.modified;
+
+  for (let index = 0; index < unstagedFiles.length; index++) {
+    const unstagedFile = unstagedFiles[index];
+    if (unstagedFile.includes(sourceFolder)) {
+      const filename = unstagedFile.split(path.sep).pop();
+      const episodeNumber = Number(filename?.split('-')[0]);
+      const commitMessage = `feat(${reign}): update tags in episode n° ${episodeNumber}`;
+      await git.add(unstagedFile);
+      await git.commit(commitMessage);
+      // eslint-disable-next-line no-console
+      console.log(commitMessage);
+    }
+  }
+}
+
+autoCommitEpisodesWithUpdatedTags('/10-guerschasp/').then();
