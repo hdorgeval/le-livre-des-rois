7babd9fec0465a8c228300781cdf2a2fa4ab8e22
hdorgeval <henri.dorgeval@outlook.fr>
2021-04-28 18:47:33 +0200

    chore(git): add script to auto git updated spelling dictionary

---
+++ src/tools/git/auto-commit-updated-spelling-dictionary.ts
@@ -0,0 +1,31 @@
+import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
+import {} from 'fs';
+
+const options: Partial<SimpleGitOptions> = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+};
+
+async function autoCommitUpdatedSpellingDictionary() {
+  // when setting all options in a single object
+  const git: SimpleGit = simpleGit(options);
+
+  const status = await git.status();
+  // eslint-disable-next-line no-console
+  console.log(status);
+  const unstagedFiles = status.modified;
+
+  for (let index = 0; index < unstagedFiles.length; index++) {
+    const unstagedFile = unstagedFiles[index];
+    if (unstagedFile.includes('ltex.dictionary')) {
+      const commitMessage = `chore(spelling): update dictionary`;
+      await git.add(unstagedFile);
+      await git.commit(commitMessage);
+      // eslint-disable-next-line no-console
+      console.log(commitMessage);
+    }
+  }
+}
+
+autoCommitUpdatedSpellingDictionary().then();
