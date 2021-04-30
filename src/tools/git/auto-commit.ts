df8239fd6d266df37cf7b739e09db70f715fb9d5
hdorgeval <henri.dorgeval@outlook.fr>
2021-04-30 23:25:58 +0200

    feat(tools): update git helpers

--- src/tools/git/auto-commit.ts
+++ src/tools/git/auto-commit.ts
@@ -8,10 +8,10 @@ import {
   autoCommitUpdatedTagsTools,
 } from '.';
 
-autoCommitUpdatedGitHelpers().then();
-autoCommitNewGitHelpers().then();
 autoCommitUpdatedMarkdownFormatter().then();
 autoCommitUpdatedLatexFiles().then();
 autoCommitUpdatedGenealogy().then();
 autoCommitUpdatedTagsTools().then();
 autoCommitNewMarkdownTags().then();
+autoCommitNewGitHelpers().then();
+autoCommitUpdatedGitHelpers().then();
