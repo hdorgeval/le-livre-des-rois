4d20ac356abeef5e69096f1bef17a14e49f12df9
hdorgeval <henri.dorgeval@outlook.fr>
2021-05-08 16:20:36 +0200

    feat(markdown): enhance markdown formatter

--- src/tools/markdown-formatter/apply-rule-on-quotation-mark.ts
+++ src/tools/markdown-formatter/apply-rule-on-quotation-mark.ts
@@ -64,15 +64,15 @@ export function splitSentencesOnStartOfQuotationMark(content: string): string {
 
 function takeLastTenWordsOf(content: string) {
   const words = content.split(' ');
-  if (words.length < 10) {
+  if (words.length < 15) {
     return content;
   }
 
   const numberOfWords = words.length;
-  return words.filter((_word, index) => index >= numberOfWords - 10).join(' ');
+  return words.filter((_word, index) => index >= numberOfWords - 15).join(' ');
 }
 
-const startOfQuotationRegex = /fit dire|en répétant|en disant|en criant|s’écrièrent|leur disant|en lui disant|disait|, disant|en ces mots|se disant|et disant|un dit|et ajouta|Il adressa ainsi la parole|se dit|dit en se|dit devant|il demanda à|demanda aux|lui demanda|lui demanderai|Il dit|ils dirent|Puis, il dit|et lui dit|Ensuite il dit|Ensuite, il dit|et il dit|a dit|et dit|Puis il dit|il se dit|il dit à|dit en lui-même|Le roi dit à|il dit aux|dit à|lui dit|lui dirent|lui diras|leur dit|dis-lui|répondit|ils s’écrièrent|en s’écriant|ajouta en s’adressant|discours|la réponse|parla ainsi|envoya son salut|reprend|répliqua|apparut en|ces paroles|réponse/i;
+const startOfQuotationRegex = /fit dire|en répétant|en disant|en criant|s’écrièrent|leur disant|leur parla|en lui disant|disait|, disant|en ces mots|se disant|et disant|un dit|et ajouta|Il adressa ainsi la parole|se dit|dit en se|dit devant|il demanda à|demanda aux|lui demanda|lui demanderai|Il dit|ils dirent|Puis, il dit|et lui dit|Ensuite il dit|Ensuite, il dit|et il dit|a dit|et dit|Puis il dit|il se dit|il dit à|dit en lui-même|Le roi dit à|il dit aux|dit à|lui dit|lui dirent|lui diras|leur dit|dis-lui|répondit|ils s’écrièrent|en s’écriant|ajouta en s’adressant|discours|la réponse|parla ainsi|envoya son salut|reprend|répliqua|apparut en|ces paroles|réponse/i;
 function isHintForStartOfQuotation(content: string): boolean {
   if (!content) {
     return false;
