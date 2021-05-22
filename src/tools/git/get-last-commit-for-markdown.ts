bb589e457285b744d0d2fd3cbfd5922b6d6830c3
hdorgeval <henri.dorgeval@outlook.fr>
2021-05-22 22:56:24 +0200

    feat(tools): add git helpers

---
+++ src/tools/git/get-last-commit-for-markdown.ts
@@ -0,0 +1,25 @@
+import simpleGit, { DefaultLogFields, ListLogLine, SimpleGit, SimpleGitOptions } from 'simple-git';
+
+const options: Partial<SimpleGitOptions> = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+};
+
+export async function getLastCommitForUpdatedContentOf(
+  filepath: string,
+): Promise<(DefaultLogFields & ListLogLine) | null> {
+  // when setting all options in a single object
+  const git: SimpleGit = simpleGit(options);
+
+  const fileHistory = await git.log({ file: filepath });
+  const mostRecent = fileHistory.all
+    .filter(
+      (history) => !history.message.includes('metadata') && !history.message.includes('metada'),
+    )
+    .shift();
+  const oldest = [...fileHistory.all].pop();
+  const latest = fileHistory.latest;
+
+  return mostRecent || oldest || latest;
+}
