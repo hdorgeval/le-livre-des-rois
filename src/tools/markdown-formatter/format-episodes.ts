db512cde4cbf18f5d8f52577e4f91a194b1f1133
hdorgeval <henri.dorgeval@outlook.fr>
2021-05-25 20:43:08 +0200

    feat(markdown): enhance markdown formatter

--- src/tools/markdown-formatter/format-episodes.ts
+++ src/tools/markdown-formatter/format-episodes.ts
@@ -18,4 +18,4 @@ function formatEpisodesIn(folder: string, start: number, end: number) {
   }
 }
 
-formatEpisodesIn('src/markdown/06-feridoun', 20, 27);
+formatEpisodesIn('src/markdown/35-bahram-gour', 1, 45);
