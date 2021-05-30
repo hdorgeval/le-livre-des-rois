4299619ecc746140dce107d8f2b538af1aca3165
hdorgeval <henri.dorgeval@outlook.fr>
2021-05-31 01:02:04 +0200

    feat(markdown): enhance markdown formatter

--- src/tools/markdown-formatter/format-episodes.ts
+++ src/tools/markdown-formatter/format-episodes.ts
@@ -18,4 +18,4 @@ function formatEpisodesIn(folder: string, start: number, end: number) {
   }
 }
 
-formatEpisodesIn('src/markdown/38-pirouz', 1, 4);
+formatEpisodesIn('src/markdown/39-balasch', 1, 4);
