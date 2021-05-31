c692d3bc5a34dd24f833be4a8f04231fc5c24274
hdorgeval <henri.dorgeval@outlook.fr>
2021-05-31 22:38:44 +0200

    feat(markdown): enhance markdown formatter

--- src/tools/markdown-formatter/format-episodes.ts
+++ src/tools/markdown-formatter/format-episodes.ts
@@ -18,4 +18,4 @@ function formatEpisodesIn(folder: string, start: number, end: number) {
   }
 }
 
-formatEpisodesIn('src/markdown/39-balasch', 1, 4);
+formatEpisodesIn('src/markdown/40-kobad-fils-de-pirouz', 1, 9);
