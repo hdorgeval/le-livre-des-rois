f451f6725f870d1e918516b3bfd7d144d6d4da1f
hdorgeval <henri.dorgeval@outlook.fr>
2021-06-01 20:27:14 +0200

    feat(markdown): enhance markdown formatter

--- src/tools/markdown-formatter/format-episodes.ts
+++ src/tools/markdown-formatter/format-episodes.ts
@@ -18,4 +18,4 @@ function formatEpisodesIn(folder: string, start: number, end: number) {
   }
 }
 
-formatEpisodesIn('src/markdown/41-nouschirwan', 1, 65);
+formatEpisodesIn('src/markdown/42-hormuzd', 1, 33);
