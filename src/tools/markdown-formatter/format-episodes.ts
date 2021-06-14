108851fa5d71855e0918719e5714b34a5d3a07ff
hdorgeval <henri.dorgeval@outlook.fr>
2021-06-14 19:28:20 +0200

    feat(markdown): enhance markdown formatter

--- src/tools/markdown-formatter/format-episodes.ts
+++ src/tools/markdown-formatter/format-episodes.ts
@@ -18,4 +18,4 @@ function formatEpisodesIn(folder: string, start: number, end: number) {
   }
 }
 
-formatEpisodesIn('src/markdown/07-minoutchehr', 7, 35);
+formatEpisodesIn('src/markdown/07-minoutchehr', 8, 35);
