36baa50b772b9a9263f50d7bb638b15335ca520e
hdorgeval <henri.dorgeval@outlook.fr>
2021-06-20 16:51:40 +0200

    feat(web): add search component in lexicon page

--- src/components/tags/tags.tsx
+++ src/components/tags/tags.tsx
@@ -1,10 +1,11 @@
 import { Tag } from './tag';
-import { AllMarkdownRemarkResponse } from '../../graphql';
+import { AllMarkdownRemarkResponse, MarkdownGroupedTag } from '../../graphql';
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import Fuse from 'fuse.js';
 
 export const Tags: React.FC = () => {
-  const data = useStaticQuery<AllMarkdownRemarkResponse>(graphql`
+  const allData = useStaticQuery<AllMarkdownRemarkResponse>(graphql`
     {
       allMarkdownRemark {
         group(field: frontmatter___tags) {
@@ -14,25 +15,79 @@ export const Tags: React.FC = () => {
       }
     }
   `);
+
+  const allTags: MarkdownGroupedTag[] = React.useMemo(() => {
+    return allData?.allMarkdownRemark?.group || [];
+  }, [allData]);
+
+  const fuzzySearch = React.useMemo(() => {
+    const options = {
+      includeScore: true,
+      findAllMatches: true,
+      threshold: 0.6,
+      keys: ['fieldValue'],
+    };
+    const search = new Fuse<MarkdownGroupedTag>(allTags, options);
+    return search;
+  }, [allTags]);
+
+  const [tags, setTags] = React.useState<MarkdownGroupedTag[]>(allTags);
+  const onSearch: React.ChangeEventHandler<HTMLInputElement> = React.useCallback(
+    (event) => {
+      const searchValue = event.target.value;
+      if (!searchValue) {
+        setTags(allTags);
+        return;
+      }
+      if (searchValue && searchValue.length === 1) {
+        setTags(
+          allTags.filter((tag) =>
+            tag.fieldValue.toLowerCase().startsWith(searchValue.toLowerCase()),
+          ),
+        );
+        return;
+      }
+      const fuzzyResult = fuzzySearch.search(searchValue);
+      const result = fuzzyResult.filter((r) => Number(r.score) <= 0.3).map((r) => r.item);
+      setTags(result);
+    },
+    [setTags, allTags, fuzzySearch],
+  );
+
   return (
-    <div id="tags-container" className="container-fluid">
-      {data.allMarkdownRemark.group
-        .sort((a, b) =>
-          a.fieldValue.replace('â', 'a').toLowerCase() >=
-          b.fieldValue.replace('â', 'a').toLowerCase()
-            ? 1
-            : -1,
-        )
-        .map((group, index) => {
-          return (
-            <Tag
-              index={index}
-              key={group.fieldValue}
-              size={group.totalCount}
-              text={group.fieldValue}
-            />
-          );
-        })}
-    </div>
+    <>
+      <div className="bg-dark text-light input-group mx-auto sticky-top w-75 mb-3">
+        <span className="bg-dark input-group-text" id="search-input">
+          <i className="bi bi-search text-light"></i>
+        </span>
+        <input
+          type="text"
+          className="form-control bg-dark text-light"
+          placeholder="Rechercher un mot dans le lexique"
+          aria-label="Rechercher un mot dans le lexique"
+          aria-describedby="search-input"
+          onChange={onSearch}
+        />
+      </div>
+      <div id="tags-container" className="container-fluid">
+        {tags
+          .sort((a, b) =>
+            a.fieldValue.replace('â', 'a').toLowerCase() >=
+            b.fieldValue.replace('â', 'a').toLowerCase()
+              ? 1
+              : -1,
+          )
+          .map((group, index) => {
+            return (
+              <Tag
+                index={index}
+                key={group.fieldValue}
+                size={group.totalCount}
+                text={group.fieldValue}
+              />
+            );
+          })}
+      </div>
+    </>
   );
 };
