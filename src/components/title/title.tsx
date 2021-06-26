02a0534e930866151380f6a6359f812430c58890
hdorgeval <henri.dorgeval@outlook.fr>
2021-06-26 14:53:48 +0200

    fix(web): set heading elements in a sequentially-descending order

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -7,6 +7,6 @@ export interface TitleProps {
 export const Title: React.FC<TitleProps> = ({ text, subtitle }) => (
   <section className="text-light text-center text-uppercase border-bottom border-bottom-1 border-secondary pb-0 mb-3">
     <h1>{text}</h1>
-    {subtitle && subtitle.length > 0 && <h4 className="text-muted">{subtitle}</h4>}
+    {subtitle && subtitle.length > 0 && <h2 className="text-muted fs-4">{subtitle}</h2>}
   </section>
 );
