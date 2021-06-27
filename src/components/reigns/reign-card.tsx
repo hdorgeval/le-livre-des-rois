7caafef692337b3bf6499315ea7ee47ea1a47fc2
thibaultboursier <boursier.thibault@gmail.com>
2021-06-27 23:05:49 +0200

    refactor(home-cards): improve styles

--- src/components/reigns/reign-card.tsx
+++ src/components/reigns/reign-card.tsx
@@ -1,6 +1,15 @@
 import { AllMarkdownRemarkResponse } from '../../graphql';
 import React from 'react';
 import { Link } from 'gatsby';
+import { CSSProperties } from 'react';
+
+const cardStyle: CSSProperties = {
+  backgroundImage:
+    "linear-gradient(to bottom, rgba(0, 0, 0, 0.8), rgba(117, 19, 93, 0.30)), url('https://content.wdl.org/10610/thumbnail/1430174992/616x510.jpg')",
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: 'cover',
+  minHeight: '280px',
+};
 
 export interface ReignCardProps {
   data: AllMarkdownRemarkResponse;
@@ -16,21 +25,17 @@ export const ReignCard: React.FC<ReignCardProps> = ({
   cardTitle,
   cardBody,
 }) => {
-  const totalNumberOfEpsiodes = data.allMarkdownRemark.totalCount;
+  const totalNumberOfEpisodes = data.allMarkdownRemark.totalCount;
 
   return (
     <Link className="nav-link mb-4 p-0" to={`/${reignSlug}/`} aria-label={reignTitle}>
-      <div className="card text-center bg-dark text-white border-secondary">
-        <div className="card-header pb-0">
-          <h3 className="card-title fs-5">{cardTitle}</h3>
+      <div className="card text-light border-secondary" style={cardStyle}>
+        <div className="card-body card-text text-light text-center d-flex flex-column align-items-center justify-content-center">
+          <h4 className="card-title">{cardTitle}</h4>
+          <p className="card-text">{cardBody}</p>
         </div>
-        <div className="card-body card-text">
-          <blockquote className="blockquote mb-0">
-            <p className="text-secondary">{cardBody}</p>
-          </blockquote>
-        </div>
-        <div className="card-footer text-muted">
-          <span>{`${totalNumberOfEpsiodes} épisode${totalNumberOfEpsiodes > 1 ? 's' : ''}`}</span>
+        <div className="card-footer text-light border-0">
+          {totalNumberOfEpisodes} épisode{totalNumberOfEpisodes > 1 ? 's' : ''}
         </div>
       </div>
     </Link>
