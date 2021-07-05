f238ae7e4f3671898efda0569a3fe46c9b7acfcf
hdorgeval <henri.dorgeval@outlook.fr>
2021-07-05 02:15:24 +0200

    fix(components/title): prevent empty h1

--- src/components/title/title.tsx
+++ src/components/title/title.tsx
@@ -5,8 +5,8 @@ export interface TitleProps {
   subtitle: string;
 }
 export const Title: React.FC<TitleProps> = ({ text, subtitle }) => (
-  <section className="text-light text-center text-uppercase border-bottom border-bottom-1 border-secondary pb-4 pt-2 mb-3">
-    <h1>{text}</h1>
+  <section className="text-light text-center text-uppercase border-bottom border-bottom-1 border-secondary pb-1 pt-2 mb-3">
+    {text && <h1>{text}</h1>}
     {subtitle && subtitle.length > 0 && <h2 className="text-muted fs-4">{subtitle}</h2>}
   </section>
 );
