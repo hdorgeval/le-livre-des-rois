29039142bb50882679a35282c2d926b406e2b26b
hdorgeval <henri.dorgeval@outlook.fr>
2021-07-08 20:53:05 +0200

    feat(tools): add git helpers

---
+++ src/tools/git/auto-commit-episodes-with-updated-ltext-settings.ts
@@ -0,0 +1,32 @@
+import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
+import path from 'path';
+
+const options: Partial<SimpleGitOptions> = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+};
+
+export async function autoCommitEpisodesWithUpdatedLtextSettings(): Promise<void> {
+  const git: SimpleGit = simpleGit(options);
+
+  const status = await git.status();
+  const unstagedFiles = status.modified;
+  for (let index = 0; index < unstagedFiles.length; index++) {
+    const unstagedFile = unstagedFiles[index];
+
+    if (unstagedFile.includes('src/markdown/')) {
+      const search = unstagedFile.match(/markdown\/\d\d-(.*)\//i);
+      const reign = search ? search[1] : 'reign';
+      const filename = unstagedFile.split(path.sep).pop();
+      const episodeNumber = Number(filename?.split('-')[0]);
+      const commitMessage = `feat(${reign}): add ltex settings metadata in episode n° ${episodeNumber}`;
+      await git.add(unstagedFile);
+      await git.commit(commitMessage);
+      // eslint-disable-next-line no-console
+      console.log(commitMessage);
+    }
+  }
+}
+
+autoCommitEpisodesWithUpdatedLtextSettings().then();
