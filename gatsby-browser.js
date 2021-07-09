f8b7a3a6a6dc856af1ed1429ff3cd5342a54e452
hdorgeval <henri.dorgeval@outlook.fr>
2021-07-09 22:57:39 +0200

    chore(gatsby): add css from leaflet-draw

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,3 +3,4 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'leaflet/dist/leaflet.js';
 import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
