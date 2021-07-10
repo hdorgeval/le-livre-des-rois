3890c7dcd8e570c94514ca8bec25d96765917850
hdorgeval <henri.dorgeval@outlook.fr>
2021-07-10 16:52:13 +0200

    refactor(components): move seo component to 'common' folder

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -2,3 +2,4 @@ export * from './genealogy';
 export * from './tag';
 export * from './leaflet';
 export * from './read-more-read-less';
+export * from './seo';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
-export * from './seo';
 export * from './title';
 export * from './common';
