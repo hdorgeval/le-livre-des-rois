2a2e479ee463f1885c69e9270689df87d58a0efc
hdorgeval <henri.dorgeval@outlook.fr>
2021-07-10 19:22:27 +0200

    feat(markdown): enhance markdown formatter

--- src/tools/markdown-formatter/format-episodes.ts
+++ src/tools/markdown-formatter/format-episodes.ts
@@ -18,4 +18,4 @@ function formatEpisodesIn(folder: string, start: number, end: number) {
   }
 }
 
-formatEpisodesIn('src/markdown/12-kaous', 5, 153);
+formatEpisodesIn('src/markdown/12-kaous', 6, 153);
