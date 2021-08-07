7fa41440d1004129e29167b4436ee75b9b84b4df
hdorgeval <henri.dorgeval@outlook.fr>
2021-08-07 19:07:58 +0200

    feat(tools/fr): update ltex helpers

--- src/tools/fr/ltex/extract-fr-rules-from-ltex-checker.ts
+++ src/tools/fr/ltex/extract-fr-rules-from-ltex-checker.ts
@@ -68,9 +68,7 @@ export const processLog = (logFile: PathLike | null): string[] => {
 };
 
 syncFrSettingsFromVscode();
-const rules = createRulesFromLtex(
-  path.join(process.cwd(), 'src/markdown/fr/40-kobad-fils-de-pirouz'),
-);
+const rules = createRulesFromLtex(path.join(process.cwd(), 'src/markdown/fr/41-nouschirwan'));
 // eslint-disable-next-line no-console
 console.log(rules);
 const replacers = rules.map((rule) => {
