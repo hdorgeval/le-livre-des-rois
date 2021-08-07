a05cae284e5c0418e931f37861463332c2176ae8
hdorgeval <henri.dorgeval@outlook.fr>
2021-08-07 18:01:23 +0200

    feat(tools/fr): update ltex helpers

--- src/tools/fr/ltex/extract-fr-rules-from-ltex-checker.ts
+++ src/tools/fr/ltex/extract-fr-rules-from-ltex-checker.ts
@@ -68,7 +68,7 @@ export const processLog = (logFile: PathLike | null): string[] => {
 };
 
 syncFrSettingsFromVscode();
-const rules = createRulesFromLtex(path.join(process.cwd(), 'src/markdown/fr/38-pirouz'));
+const rules = createRulesFromLtex(path.join(process.cwd(), 'src/markdown/fr/39-balasch'));
 // eslint-disable-next-line no-console
 console.log(rules);
 const replacers = rules.map((rule) => {
