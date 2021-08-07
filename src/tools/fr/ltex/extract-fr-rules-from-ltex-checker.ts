0918983da7e1893470b07e314cc963ccddce1d45
hdorgeval <henri.dorgeval@outlook.fr>
2021-08-07 14:37:40 +0200

    feat(tools/fr): update ltex helpers

--- src/tools/fr/ltex/extract-fr-rules-from-ltex-checker.ts
+++ src/tools/fr/ltex/extract-fr-rules-from-ltex-checker.ts
@@ -68,7 +68,9 @@ export const processLog = (logFile: PathLike | null): string[] => {
 };
 
 syncFrSettingsFromVscode();
-const rules = createRulesFromLtex(path.join(process.cwd(), 'src/markdown/fr/23-schapour'));
+const rules = createRulesFromLtex(
+  path.join(process.cwd(), 'src/markdown/fr/30-schapour-dhoul-aktaf'),
+);
 // eslint-disable-next-line no-console
 console.log(rules);
 const replacers = rules.map((rule) => {
