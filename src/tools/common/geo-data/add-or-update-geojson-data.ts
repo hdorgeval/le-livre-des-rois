b3330954bb973325f96918bd2fbaa37e6426f53f
hdorgeval <henri.dorgeval@outlook.fr>
2021-08-29 22:10:03 +0200

    feat(tools): add geo-data helpers

---
+++ src/tools/common/geo-data/add-or-update-geojson-data.ts
@@ -0,0 +1,6 @@
+import { addOrUpdateGeoJsonDataOf } from '.';
+
+// addOrUpdateGeoJsonDataOf('Amol').inCountry('IR');
+// addOrUpdateGeoJsonDataOf('Ahvaz').inCountry('IR');
+// addOrUpdateGeoJsonDataOf('Alep').inCountry('SY');
+addOrUpdateGeoJsonDataOf('Balkh').inCountry('AF');
