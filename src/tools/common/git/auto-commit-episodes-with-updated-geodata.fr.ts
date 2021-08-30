2f73f4ea5a122bb2a0972f29c7ea30eef41db99f
hdorgeval <henri.dorgeval@outlook.fr>
2021-08-30 22:01:05 +0200

    chore(npm): add script to auto commit episodes with updated geodata

---
+++ src/tools/common/git/auto-commit-episodes-with-updated-geodata.fr.ts
@@ -0,0 +1,35 @@
+import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
+import path from 'path';
+import {} from 'fs';
+
+const options: Partial<SimpleGitOptions> = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+};
+
+async function autoCommitEpisodesWithUpdatedGeoData() {
+  // when setting all options in a single object
+  const git: SimpleGit = simpleGit(options);
+
+  const status = await git.status();
+  const unstagedFiles = status.modified;
+
+  for (let index = 0; index < unstagedFiles.length; index++) {
+    const unstagedFile = unstagedFiles[index];
+    if (unstagedFile.includes('src/markdown/fr/')) {
+      const search = unstagedFile.match(/markdown\/fr\/\d\d-(.*)\//i);
+      const reign = search ? search[1] : 'reign';
+
+      const filename = unstagedFile.split(path.sep).pop();
+      const episodeNumber = Number(filename?.split('-')[0]);
+      const commitMessage = `feat(${reign}/fr): update geodata in episode n° ${episodeNumber}`;
+      await git.add(unstagedFile);
+      await git.commit(commitMessage);
+      // eslint-disable-next-line no-console
+      console.log(commitMessage);
+    }
+  }
+}
+
+autoCommitEpisodesWithUpdatedGeoData().then();
