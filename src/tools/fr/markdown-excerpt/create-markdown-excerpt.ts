fefdd2edcc757fb2958e6ac9c9bca3296649a311
hdorgeval <henri.dorgeval@outlook.fr>
2021-09-07 01:12:46 +0200

    feat(tools): start implementation of excerpt creation script

---
+++ src/tools/fr/markdown-excerpt/create-markdown-excerpt.ts
@@ -0,0 +1,7 @@
+const filepath = process.env['CURRENT_MD_FILEPATH'] || '';
+const selectedText = process.env['CURRENT_MD_SELECTED_TEXT'] || '';
+
+// eslint-disable-next-line no-console
+console.log(filepath);
+// eslint-disable-next-line no-console
+console.log(selectedText);
