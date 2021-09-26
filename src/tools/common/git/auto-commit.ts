ede244e02ed3333c441a9d3fe96d3022b2364c17
hdorgeval <henri.dorgeval@outlook.fr>
2021-09-27 00:51:02 +0200

    feat(tools): update git helpers

--- src/tools/common/git/auto-commit.ts
+++ src/tools/common/git/auto-commit.ts
@@ -23,34 +23,40 @@ import {
   autoCommitUpdatedFrenchEpisodes,
   autoCommitUpdatedLaunchJson,
   autoCommitNewEnglishWarnerEpisodes,
+  autoCommitUpdatedEnglishWarnerEpisodes,
 } from '.';
 
 async function autoCommit() {
   // english - warner - version
   await autoCommitNewEnglishWarnerEpisodes().then();
+  await autoCommitUpdatedEnglishWarnerEpisodes().then();
   // french - mohl - version
   await autoCommitNewFrenchEpisodes().then();
   await autoCommitUpdatedFrenchEpisodes().then();
   await autoCommitUpdatedFrenchMarkdownFormatter().then();
-  await autoCommitUpdatedLatexFiles().then();
-  await autoCommitAddedLatexDictionaryFiles().then();
-  await autoCommitAddedLatexFalsePositivesFiles().then();
   await autoCommitUpdatedFrenchGenealogy().then();
   await autoCommitUpdatedFrenchTagsTools().then();
   await autoCommitNewFrenchMarkdownTags().then();
   await autoCommitUpdatedFrenchMarkdownTags().then();
-  await autoCommitNewGitHelpers().then();
-  await autoCommitUpdatedGitHelpers().then();
   await autoCommitUpdatedFrenchMarkdownTagsHelpers().then();
-  await autoCommitNewGeoData().then();
-  await autoCommitUpdatedGeoData().then();
-  await autoCommitNewGeoDataHelpers().then();
-  await autoCommitUpdatedGeoDataHelpers().then();
   await autoCommitNewFrenchLtexHelpers().then();
   await autoCommitUpdatedFrenchLtexHelpers().then();
   await autoCommitNewFrenchFrontmatterHelpers().then();
   await autoCommitUpdatedFrenchFrontmatterHelpers().then();
   await autoCommitUpdatedFrenchStats().then();
+  // ltex files
+  await autoCommitUpdatedLatexFiles().then();
+  await autoCommitAddedLatexDictionaryFiles().then();
+  await autoCommitAddedLatexFalsePositivesFiles().then();
+  // Git helpers
+  await autoCommitNewGitHelpers().then();
+  await autoCommitUpdatedGitHelpers().then();
+  // Geo data
+  await autoCommitNewGeoData().then();
+  await autoCommitUpdatedGeoData().then();
+  await autoCommitNewGeoDataHelpers().then();
+  await autoCommitUpdatedGeoDataHelpers().then();
+  // vscode
   await autoCommitUpdatedLaunchJson().then();
 }
 
