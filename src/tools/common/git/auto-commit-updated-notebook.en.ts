c1d2ca101d619049950c541bc3d46fdecb2c502b
hdorgeval <henri.dorgeval@outlook.fr>
2021-10-09 17:46:14 +0200

    feat(tools): add git helpers

---
+++ src/tools/common/git/auto-commit-updated-notebook.en.ts
@@ -0,0 +1,25 @@
+import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
+
+const options: Partial<SimpleGitOptions> = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+};
+
+export async function autoCommitUpdatedEnglishNotebooks(): Promise<void> {
+  const git: SimpleGit = simpleGit(options);
+
+  const status = await git.status();
+  const unstagedFiles = status.modified;
+  for (let index = 0; index < unstagedFiles.length; index++) {
+    const unstagedFile = unstagedFiles[index];
+
+    if (unstagedFile.includes('docs/books/en/warner/shahnama.ipynb')) {
+      const commitMessage = `jupiter(shahnama/en): update jupiter notebook for data collecting and processing of warner edition`;
+      await git.add(unstagedFile);
+      await git.commit(commitMessage);
+      // eslint-disable-next-line no-console
+      console.log(commitMessage);
+    }
+  }
+}
