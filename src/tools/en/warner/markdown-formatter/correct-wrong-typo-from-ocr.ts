704c7a28f7c9d28d7c0dae6dd91ed55763fd12ec
hdorgeval <henri.dorgeval@outlook.fr>
2021-10-09 21:58:12 +0200

    feat(markdown/en): enhance markdown formatter for english warner version

--- src/tools/en/warner/markdown-formatter/correct-wrong-typo-from-ocr.ts
+++ src/tools/en/warner/markdown-formatter/correct-wrong-typo-from-ocr.ts
@@ -1,6 +1,10 @@
 export function correctWrongTypoFromOcr(content: string): string {
   let result = content;
-  [(content: string) => content.replace(/ , /g, ', ')].forEach((format) => {
+  [
+    (content: string) => content.replace(/ , /g, ', '),
+    (content: string) => content.replace(/, „\n\n/g, ',\n\n'),
+    (content: string) => content.replace(/, \[\n\n/g, ',\n\n'),
+  ].forEach((format) => {
     result = format(result);
   });
   return result;
