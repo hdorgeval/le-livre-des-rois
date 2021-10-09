12b3948e427ce206ce48f11aac4ef277569951e6
hdorgeval <henri.dorgeval@outlook.fr>
2021-10-09 15:24:45 +0200

    feat(tools): add git helpers

---
+++ src/tools/common/git/auto-commit-updated-vscode-extension-settings.ts
@@ -0,0 +1,27 @@
+import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
+import {} from 'fs';
+
+const options: Partial<SimpleGitOptions> = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+};
+
+export async function autoCommitUpdatedExtensionJson(): Promise<void> {
+  // when setting all options in a single object
+  const git: SimpleGit = simpleGit(options);
+
+  const status = await git.status();
+  const unstagedFiles = status.modified;
+
+  for (let index = 0; index < unstagedFiles.length; index++) {
+    const unstagedFile = unstagedFiles[index];
+    if (unstagedFile.endsWith('.vscode/extensions.json')) {
+      const commitMessage = `chore(vscode): update extensions settings`;
+      await git.add(unstagedFile);
+      await git.commit(commitMessage);
+      // eslint-disable-next-line no-console
+      console.log(commitMessage);
+    }
+  }
+}
