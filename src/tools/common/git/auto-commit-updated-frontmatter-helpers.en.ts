f92ea937da8e124e60bbdde324c71f33866b4fad
hdorgeval <henri.dorgeval@outlook.fr>
2021-10-09 20:41:20 +0200

    feat(tools): add git helpers

---
+++ src/tools/common/git/auto-commit-updated-frontmatter-helpers.en.ts
@@ -0,0 +1,54 @@
+import simpleGit, { SimpleGit, SimpleGitOptions } from 'simple-git';
+import {} from 'fs';
+
+const options: Partial<SimpleGitOptions> = {
+  baseDir: process.cwd(),
+  binary: 'git',
+  maxConcurrentProcesses: 6,
+};
+
+export async function autoCommitUpdatedEnglishFrontmatterHelpers(): Promise<void> {
+  // when setting all options in a single object
+  const git: SimpleGit = simpleGit(options);
+
+  const status = await git.status();
+  const unstagedFiles = status.modified;
+
+  let hasUpdatedFiles = false;
+  for (let index = 0; index < unstagedFiles.length; index++) {
+    const unstagedFile = unstagedFiles[index];
+    if (unstagedFile.includes('/tools/en/markdown-frontmatter/')) {
+      hasUpdatedFiles = true;
+      await git.add(unstagedFile);
+    }
+  }
+  if (hasUpdatedFiles) {
+    const commitMessage = `feat(tools/en): update markdown-frontmatter helpers`;
+    // eslint-disable-next-line no-console
+    console.log(commitMessage);
+    await git.commit(commitMessage);
+  }
+}
+
+export async function autoCommitNewEnglishFrontmatterHelpers(): Promise<void> {
+  // when setting all options in a single object
+  const git: SimpleGit = simpleGit(options);
+
+  const status = await git.status();
+  const unstagedFiles = status.not_added;
+
+  let hasUpdatedFiles = false;
+  for (let index = 0; index < unstagedFiles.length; index++) {
+    const unstagedFile = unstagedFiles[index];
+    if (unstagedFile.includes('/tools/en/markdown-frontmatter/')) {
+      hasUpdatedFiles = true;
+      await git.add(unstagedFile);
+    }
+  }
+  if (hasUpdatedFiles) {
+    const commitMessage = `feat(tools/en): add markdown-frontmatter helpers`;
+    // eslint-disable-next-line no-console
+    console.log(commitMessage);
+    await git.commit(commitMessage);
+  }
+}
