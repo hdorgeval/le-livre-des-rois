980d2b1afb84739e7e5743696510b2c229db94eb
hdorgeval <henri.dorgeval@outlook.fr>
2021-10-16 12:14:37 +0200

    feat(tools): enhance common markdown formatter tools

---
+++ src/tools/common/markdown-formatter/check-regular-expressions.test.ts
@@ -0,0 +1,28 @@
+import 'jest';
+import { hasMissingEscapeCharacterOnBracket } from '.';
+
+test(`Should detect regex has no un-escaped [ character`, () => {
+  // Given
+  // prettier-ignore
+  // eslint-disable-next-line no-useless-escape
+  const line = "(content: string) => content.replace(/, v\. [1,2,3][1,2,3]\n/g, ',\n'),";
+
+  // When
+  const result = hasMissingEscapeCharacterOnBracket(line);
+
+  // Then
+  expect(result).toBe(false);
+});
+
+test(`Should detect regex has un-escaped [ character`, () => {
+  // Given
+  // prettier-ignore
+  // eslint-disable-next-line no-useless-escape
+  const line = "(content: string) => content.replace(/, v\. [] [1,2,3][1,2,3]\n/g, ',\n'),";
+
+  // When
+  const result = hasMissingEscapeCharacterOnBracket(line);
+
+  // Then
+  expect(result).toBe(true);
+});
--- src/tools/common/markdown-formatter/check-regular-expressions.ts
+++ src/tools/common/markdown-formatter/check-regular-expressions.ts
@@ -63,6 +63,16 @@ export function ensureParentethisCharactersAreEscapedIn(filename: PathLike): voi
     throw new Error(`file '${filename}' has wrong regular expressions`);
   }
 }
+export function hasMissingEscapeCharacterOnBracket(line: string): boolean {
+  const rawRegex = line.split('(/')[1].split('/g')[0];
+  const sanitizedRawRegex = rawRegex.replace(/\[1,2,3\]/g, '');
+  if (sanitizedRawRegex.match(/[^\\]\[/)) {
+    // eslint-disable-next-line no-console
+    console.log(`regex '/${line}/' is missing escape character`);
+    return true;
+  }
+  return false;
+}
 
 export function ensureBracketCharactersAreEscapedIn(filename: PathLike): void {
   const lines = readAllLinesInFile(filename);
@@ -70,15 +80,7 @@ export function ensureBracketCharactersAreEscapedIn(filename: PathLike): void {
   const hasUnescapedCharacters = lines
     .filter((line) => line.includes('.replace(/'))
     .filter((line) => line.includes('['))
-    .map((line) => line.split('(/')[1].split('/g')[0])
-    .some((line) => {
-      if (line.match(/[^\\]\[/)) {
-        // eslint-disable-next-line no-console
-        console.log(`regex '/${line}/' is missing escape character`);
-        return true;
-      }
-      return false;
-    });
+    .some((line) => hasMissingEscapeCharacterOnBracket(line));
 
   if (hasUnescapedCharacters) {
     throw new Error(`file '${filename}' has wrong regular expressions`);
